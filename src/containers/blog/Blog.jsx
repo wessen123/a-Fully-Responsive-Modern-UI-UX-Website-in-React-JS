d51bdfa04ccd9e951469a9a4741317f4ac84823f
wondwessen <wessen333@gmail.com>
2022-09-13 18:07:11 +0300

    Creating & Styling all Components from Figma design

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Article from '../../components/article/Article';
+//import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import './blog.css';
+
+const Blog = () => (
+  <div>
+    Blog
+  </div>
+);
+
+export default Blog;
